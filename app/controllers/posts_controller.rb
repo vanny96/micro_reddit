10aed6ed52a078ebd74997a18183df3469aa3cb8
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-05-11 11:25:40 +0200

    Add incomplete 'create post' function, still needs users to be created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+
+    @post.valid?
+    puts @post.errors.messages
+    
+    if @post.valid?
+      @post.save
+      redirect_to @post
+    else
+      render "new"
+    end
+  end
 end
